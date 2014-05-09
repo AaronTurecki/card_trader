dc6d9913f27847ee6334bc9d9115f175a6f0adf0
Aaron Turecki <aaronturecki@gmail.com>
2014-05-08 19:41:34 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CardTrader::Application.config.secret_key_base = '55c5c5c957cc3c7b092158c4e8be88bd39fb312e521b5c2d753d7db6e7413600e2e33f6adb02369c4abe2868352de7964219e8bbcf632446fff38539845152bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CardTrader::Application.config.session_store :cookie_store, key: '_card_trader_session'

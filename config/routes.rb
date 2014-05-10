027cb1b9ccc010fe238c20ef7d73e2045128fb16
Aaron Turecki <aaronturecki@gmail.com>
2014-05-09 18:12:14 -0700

    alsdjfld

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 CardTrader::Application.routes.draw do
 
-<<<<<<< HEAD
   get "users/new"
   resources :cards
   root 'cards#index'
 
   match '/signup',  to: 'users#new',            via: 'get'
-
-=======
-  resources :cards
-  root 'cards#index'
-
->>>>>>> 21fb8ea0cd78e72f0a7fd1692b27069c8bf91b9f
 
 end

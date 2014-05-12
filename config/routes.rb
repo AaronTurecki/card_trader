2fe6ad67d5e1b3c846f5323cd0e3905d294dd00e
Aaron Turecki <aaronturecki@gmail.com>
2014-05-12 01:45:06 -0700

    migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 CardTrader::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   resources :cards
   root 'cards#index'
 
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
 end

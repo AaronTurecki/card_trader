e6388e5d6b77a0b4245be22140e96724dec3872f
Aaron Turecki <aaronturecki@gmail.com>
2014-05-12 17:03:21 -0700

    alsdjflk

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
+  def index
+    @users = User.all
+  end
 
+  def show
+    @user = User.find(params[:id])
+  end
 
   def new
     @user = User.new
@@ -17,14 +24,6 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
-  end
-
-    def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
   end
 
   def create
